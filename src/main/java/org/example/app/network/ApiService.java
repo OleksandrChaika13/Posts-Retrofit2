c5e50358b1340ecddb1d0b30f15b5fea721a277e
OleksandrChaika13 <axelrod1347@gmail.com>
2023-08-30 18:21:13 +0300

    Added "network" package and API classes

---
+++ src/main/java/org/example/app/network/ApiService.java
@@ -0,0 +1,21 @@
+package org.example.app.network;
+
+import org.example.app.entity.Post;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+import java.util.List;
+
+public interface ApiService {
+
+    @GET("posts")
+    Call<List<Post>> getUsers();
+
+    @GET("posts/{id}")
+    Call<Post> getUserById(@Path("id") int id);
+
+    @GET("posts")
+    Call<List<Post>> getFirstTenPosts(@Query("_limit") int limit);
+}
